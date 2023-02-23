8a948565d02a84cbe91bacfbf7ffc0a99307eaeb
denis <74872743+phw020410@users.noreply.github.com>
2023-02-23 14:11:20 +0000

    What is Autowired & Qualifier

--- demo/src/main/java/com/example/demo/car/Car.java
+++ demo/src/main/java/com/example/demo/car/Car.java
@@ -1,22 +1,27 @@
 package com.example.demo.car;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.example.demo.Tire;
 
 public class Car {
     @Autowired
+    @Qualifier("americaTire")
     private Tire tire;
     
-    public Car() {}
+    public Car() {
+        System.out.println("Constructer");
+    }
 
     public Tire getTire() {
         return this.tire;
     }
 
-    // public void setTire(Tire tire) {
-    //     this.tire = tire;
-    // };
+    public void setTire(Tire tire) {
+        System.out.println("setTire");
+        this.tire = tire;
+    };
 
     public void getBrand() {
         tire.getBrand();
